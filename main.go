40d2ae65524bfeeaad3147a892d719fc76119b18
chenzhihang <zh_chen@droidhang.com>
2021-11-10 18:44:45 +0800

    定时器功能增加

--- main.go
+++ main.go
@@ -9,9 +9,33 @@ import (
 
 func main() {
 	for i := 0; i < 100000; i++ {
-		timer.Add("test"+strconv.Itoa(i), 5, func() {
+		timer.Add("test0"+strconv.Itoa(i), 5, func() {
 			time.Sleep(time.Second * 2)
-			log.Println("你好世界" + strconv.Itoa(i))
+			log.Println("你好世界a" + strconv.Itoa(i))
+		})
+	}
+	for i := 0; i < 100000; i++ {
+		timer.Add("test1"+strconv.Itoa(i), 10, func() {
+			time.Sleep(time.Second * 2)
+			log.Println("你好世界b" + strconv.Itoa(i))
+		})
+	}
+	for i := 0; i < 100000; i++ {
+		timer.Add("test2"+strconv.Itoa(i), 15, func() {
+			time.Sleep(time.Second * 2)
+			log.Println("你好世界c" + strconv.Itoa(i))
+		})
+	}
+	for i := 0; i < 100000; i++ {
+		timer.Add("test3"+strconv.Itoa(i), 20, func() {
+			time.Sleep(time.Second * 2)
+			log.Println("你好世界d" + strconv.Itoa(i))
+		})
+	}
+	for i := 0; i < 100000; i++ {
+		timer.Add("test4"+strconv.Itoa(i), 25, func() {
+			time.Sleep(time.Second * 2)
+			log.Println("你好世界e" + strconv.Itoa(i))
 		})
 	}
 
