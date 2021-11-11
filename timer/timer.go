ea39a616adec6cf380cb047a104cd15d4ae94949
chenzhihang <zh_chen@droidhang.com>
2021-11-11 15:39:30 +0800

    定时器功能增加

--- timer/timer.go
+++ timer/timer.go
@@ -7,13 +7,13 @@ import (
 
 // taskEntity 时间轮任务实体
 type taskEntity struct {
-	duration int64  // 触发时间戳
-	handle   func() // 注册回调函数
+	expireTime int64  // 触发时间戳
+	handle     func() // 注册回调函数
 }
 
 // Timer 时间轮定时器
 type Timer struct {
-	tasks map[string]taskEntity // 任务 任务名字->任务实体
+	tasks map[string]*taskEntity // 任务 任务名字->任务实体
 
 	stopC chan struct{} // 定时器停止channel
 }
@@ -23,7 +23,7 @@ var newTimer Timer // 全局唯一的定时器变量
 // 全局变量初始化
 func init() {
 	newTimer = Timer{
-		tasks: make(map[string]taskEntity),
+		tasks: make(map[string]*taskEntity),
 		stopC: make(chan struct{}),
 	}
 }
@@ -34,9 +34,9 @@ func Add(taskName string, duration int64, f func()) bool {
 		return false
 	}
 
-	newTimer.tasks[taskName] = taskEntity{
-		duration: time.Now().Unix() + duration,
-		handle:   f,
+	newTimer.tasks[taskName] = &taskEntity{
+		expireTime: time.Now().Unix() + duration,
+		handle:     f,
 	}
 
 	return true
@@ -60,7 +60,7 @@ func QueryTask(taskName string) map[string]int64 {
 		return nil
 	}
 
-	return map[string]int64{taskName: task.duration - time.Now().Unix()}
+	return map[string]int64{taskName: task.expireTime - time.Now().Unix()}
 }
 
 // QueryAllTasks 查询所有任务的剩余时间
@@ -68,7 +68,7 @@ func QueryAllTasks() map[string]int64 {
 	returnMap := make(map[string]int64)
 
 	for taskName, taskEntity := range newTimer.tasks {
-		returnMap[taskName] = taskEntity.duration - time.Now().Unix()
+		returnMap[taskName] = taskEntity.expireTime - time.Now().Unix()
 	}
 
 	return returnMap
@@ -83,7 +83,7 @@ func Start() {
 			select {
 			case <-ticker.C:
 				for taskName, task := range newTimer.tasks {
-					if time.Now().Unix() >= task.duration {
+					if time.Now().Unix() >= task.expireTime {
 						go task.handle()
 						delete(newTimer.tasks, taskName)
 					}
